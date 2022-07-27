c0a719dd88447dfa30ff8ce6aaa6b834136e2152
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-27 22:16:32 +0200

    finish: milestone 1

---
+++ js/main.js
@@ -0,0 +1,183 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        contacts: [
+            {
+                'name': 'Michele',
+                'image': 'img/avatar_1.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Sofia',
+                'image': 'img/avatar_io.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Samuele',
+                'image': 'img/avatar_3.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Alessandro B',
+                'image': 'img/avatar_4.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Alessandro L',
+                'image': 'img/avatar_5.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Claudia',
+                'image': 'img/avatar_6.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Federico',
+                'image': 'img/avatar_7.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+            {
+                'name': 'Davide',
+                'image': 'img/avatar_8.jpg',
+                'visible': true,
+                'messages': [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    }, 
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    } 
+                ],
+            },
+        ]
+    }
+})
